f5a2b515be8d37c0484b16d2b2eca5461b8526d8
Antony Santos <antonyc828@gmail.com>
2020-07-08 22:54:16 -0300

    fix: set position fixed in modal

--- src/components/ModalProduto.vue
+++ src/components/ModalProduto.vue
@@ -11,16 +11,16 @@
         <h2 class="modal_titulo">{{ produto.nome }}</h2>
         <p>{{ produto.descricao }}</p>
         <button class="modal_btn">Adicionar Item</button>
-      </div>
 
-      <div class="avaliacoes">
-        <h2 class="avaliacoes_subtitulo">Avaliações</h2>
-        <ul>
-          <li class="avaliacao" v-for="avaliacao in produto.reviews" :key="avaliacao.id">
-            <p class="avaliacao_descricao">{{ avaliacao.descricao }}</p>
-            <p class="avaliacao_usuario">{{ avaliacao.nome }} | {{ avaliacao.estrelas }} estrelas</p>
-          </li>
-        </ul>
+        <div class="avaliacoes">
+          <h2 class="avaliacoes_subtitulo">Avaliações</h2>
+          <ul class="container-avaliacoes">
+            <li class="avaliacao" v-for="avaliacao in produto.reviews" :key="avaliacao.id">
+              <p class="avaliacao_descricao">{{ avaliacao.descricao }}</p>
+              <p class="avaliacao_usuario">{{ avaliacao.nome }} | {{ avaliacao.estrelas }} estrelas</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -71,13 +71,13 @@ export default {
   position: fixed;
   width: 100%;
   height: 100vh;
-  left: 0px;
-  top: 0px;
+  left: 0;
+  top: 0;
 }
 
 .modal {
-  position: absolute;
-  left: 0px;
+  position: fixed;
+  left: 0;
   top: 0;
   width: 100%;
   display: flex;
@@ -90,30 +90,22 @@ export default {
   position: relative;
   z-index: 1;
   background: linear-gradient(to right, transparent 250px, white 250px);
-  display: grid;
-  display: grid;
-  gap: 50px;
-  align-items: end;
   padding: 50px 50px 50px 0;
-}
-
-.modal_img {
-  grid-column: 1;
-  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.1), 0px 4px 10px rgba(0, 0, 0, 0.2);
+  display: flex;
 }
 
 .modal_img img {
   max-width: 300px;
-  display: block;
+  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.1), 0px 4px 10px rgba(0, 0, 0, 0.2);
 }
 
 .modal_dados {
-  grid-column: 2;
   max-width: 600px;
+  margin-left: 5%;
 }
 
 .modal .avaliacoes {
-  grid-column: 2;
+  margin-top: 8%;
 }
 
 .modal_titulo {
@@ -122,7 +114,7 @@ export default {
 
 .modal_btn {
   background-color: black;
-  margin-top: 80px;
+  margin-top: 5%;
   cursor: pointer;
   color: white;
   border: none;
@@ -139,6 +131,22 @@ export default {
 
 .avaliacoes_subtitulo {
   font-size: 1.7rem;
+}
+
+.container-avaliacoes {
+  max-height: 210px;
+  overflow-y: auto;
+}
+
+.container-avaliacoes::-webkit-scrollbar {
+  width: 4px;
+}
+.container-avaliacoes::-webkit-scrollbar-thumb {
+  background-color: gray;
+  border-radius: 4px;
+}
+.container-avaliacoes::-webkit-scrollbar-track {
+  background-color: transparent;
 }
 
 .avaliacao {
